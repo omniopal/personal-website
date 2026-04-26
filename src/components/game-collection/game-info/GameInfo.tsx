83079aa3c797b182847c015f0aa299557ef5d188
omniopal <jake_123_abc@hotmail.com>
2026-04-25 19:47:54 -0600

    Show 3D and Contents buttons on mobile, WIP styles/layout

--- src/components/game-collection/game-info/GameInfo.tsx
+++ src/components/game-collection/game-info/GameInfo.tsx
@@ -47,99 +47,119 @@ export const GameInfo: React.FC<GameInfoProps> = ({
     const [isContentsModalOpened, { open: openContentsModal, close: closeContentsModal }] = useDisclosure(false);
 
     return (
-        <div className={(clsx("game-info", isVeryLastGame && "very-last-game-info"))}>
-            <div className="image-and-title">
-                <div className="image-container">
-                    <img className="game-image" src={`/images/${filePath}.webp`} />
+        <>
+            <div className={(clsx("game-info", isVeryLastGame && "very-last-game-info"))}>
+                <div className="image-and-title">
+                    <div className="image-container">
+                        <img className="game-image" src={`/images/${filePath}.webp`} />
+                    </div>
+                    <div className="title-and-chips">
+                        <h2>{text}</h2>
+                        {gameReleaseDate && <div className="game-release-date">{gameReleaseDate}</div>}
+                        {!isSmallBreakpoint && 
+                            <div className="chips">
+                                <Chip label={physicalGameType} color="success" variant={chipInfo.hasGame ? "filled" : "outlined"} />
+                                <Chip label={boxType} color="success" variant={chipInfo.hasBox ? "filled" : "outlined"} />
+                                {chipInfo.hasManual !== undefined && <Chip label="Manual" color="success" variant={chipInfo.hasManual ? "filled" : "outlined"} />}
+                                <Chip label="Played" color="success" variant={chipInfo.hasPlayed ? "filled" : "outlined"} />
+                                {chipInfo.hasGame && <GameImageModalButton text={`My copy of ${text}`} image={personalCopyImage} />}
+                            </div>
+                        }
+                    </div>
+                    {!isSmallBreakpoint && (hasGlb || hasContentsPic) &&
+                        <Stack 
+                            style={{
+                                marginLeft: 'auto',
+                                paddingLeft: '16px',
+                            }}
+                        >
+                            {hasGlb &&
+                                <Button
+                                    color="black"
+                                    leftSection={<RotateIcon size={20} />}
+                                    onClick={open3DModal}
+                                >
+                                    View 3D
+                                </Button>
+                            }
+                            {hasContentsPic && 
+                                <Button
+                                    color="black"
+                                    leftSection={<BoxOpenIcon size={20} />}
+                                    onClick={openContentsModal}
+                                >
+                                    View contents
+                                </Button>
+                            }
+                        </Stack>
+                    }
                 </div>
-                <div className="title-and-chips">
-                    <h2>{text}</h2>
-                    {gameReleaseDate && <div className="game-release-date">{gameReleaseDate}</div>}
-                    {!isSmallBreakpoint && 
-                        <div className="chips">
+                {isSmallBreakpoint && 
+                    <div className="chips-small-breakpoint">
+                        <div className="chip-space">
                             <Chip label={physicalGameType} color="success" variant={chipInfo.hasGame ? "filled" : "outlined"} />
                             <Chip label={boxType} color="success" variant={chipInfo.hasBox ? "filled" : "outlined"} />
                             {chipInfo.hasManual !== undefined && <Chip label="Manual" color="success" variant={chipInfo.hasManual ? "filled" : "outlined"} />}
                             <Chip label="Played" color="success" variant={chipInfo.hasPlayed ? "filled" : "outlined"} />
-                            {chipInfo.hasGame && <GameImageModalButton text={`My copy of ${text}`} image={personalCopyImage} />}
-                        </div>
-                    }
-                </div>
-                {!isSmallBreakpoint && (hasGlb || hasContentsPic) &&
-                    <Stack 
-                        style={{
-                            marginLeft: 'auto',
-                            paddingLeft: '16px',
-                        }}
-                    >
-                        {hasGlb &&
-                            <Button
-                                color="black"
-                                leftSection={<RotateIcon size={20} />}
-                                onClick={open3DModal}
-                            >
-                                View 3D
-                            </Button>
-                        }
-                        {hasContentsPic && 
-                            <Button
-                                color="black"
-                                leftSection={<BoxOpenIcon size={20} />}
-                                onClick={openContentsModal}
-                            >
-                                View contents
-                            </Button>
-                        }
-                        <Modal
-                            opened={is3DModalOpened}
-                            onClose={close3DModal}
-                            title={`My copy of ${text}`}
-                            centered
-                            size="100%"
-                            styles={{
-                                title: {
-                                    fontWeight: 'bold',
-                                    fontSize: '20px',
-                                },
-                            }}
-                        >
-                            <GameModelViewer filePath={filePath} />
-                        </Modal>
-                        <Modal
-                            opened={isContentsModalOpened}
-                            onClose={closeContentsModal}
-                            title={`Contents of ${text}`}
-                            centered
-                            size="auto"
-                            styles={{
-                                title: {
-                                    fontWeight: 'bold',
-                                    fontSize: '20px',
-                                },
-                            }}
-                        >
-                            <img
-                                className="contents-image"
-                                src={`https://pub-1c3c24ac600e4d4daf14cd109a0897f1.r2.dev/${filePath}.jpg`}
-                                alt={`An image of the contents inside my copy of ${text}`}
-                            />
-                        </Modal>
-                    </Stack>
-                }
-            </div>
-            {isSmallBreakpoint && 
-                <div className="chips-small-breakpoint">
-                    <div className="chip-space">
-                        <Chip label={physicalGameType} color="success" variant={chipInfo.hasGame ? "filled" : "outlined"} />
-                        <Chip label={boxType} color="success" variant={chipInfo.hasBox ? "filled" : "outlined"} />
-                        {chipInfo.hasManual !== undefined && <Chip label="Manual" color="success" variant={chipInfo.hasManual ? "filled" : "outlined"} />}
-                        <Chip label="Played" color="success" variant={chipInfo.hasPlayed ? "filled" : "outlined"} />
-                        <div className="modal-button">
-                            {chipInfo.hasGame && <GameImageModalButton text={`My copy of ${text}`} image={personalCopyImage} />}
+                            <div className="modal-button">
+                                {chipInfo.hasGame && <GameImageModalButton text={`My copy of ${text}`} image={personalCopyImage} />}
+                            </div>
+                            {hasGlb &&
+                                <Button
+                                    color="black"
+                                    leftSection={<RotateIcon size={20} />}
+                                    onClick={open3DModal}
+                                >
+                                    View 3D
+                                </Button>
+                            }
+                            {hasContentsPic && 
+                                <Button
+                                    color="black"
+                                    leftSection={<BoxOpenIcon size={20} />}
+                                    onClick={openContentsModal}
+                                >
+                                    View contents
+                                </Button>
+                            }
                         </div>
                     </div>
-                </div>
-            }
-        </div>
+                }
+            </div>
+            <Modal
+                opened={is3DModalOpened}
+                onClose={close3DModal}
+                title={`My copy of ${text}`}
+                centered
+                size="100%"
+                styles={{
+                    title: {
+                        fontWeight: 'bold',
+                        fontSize: '20px',
+                    },
+                }}
+            >
+                <GameModelViewer filePath={filePath} />
+            </Modal>
+            <Modal
+                opened={isContentsModalOpened}
+                onClose={closeContentsModal}
+                title={`Contents of ${text}`}
+                centered
+                size="auto"
+                styles={{
+                    title: {
+                        fontWeight: 'bold',
+                        fontSize: '20px',
+                    },
+                }}
+            >
+                <img
+                    className="contents-image"
+                    src={`https://pub-1c3c24ac600e4d4daf14cd109a0897f1.r2.dev/${filePath}.jpg`}
+                    alt={`An image of the contents inside my copy of ${text}`}
+                />
+            </Modal>
+        </>
     )
 }
